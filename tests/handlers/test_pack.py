cea68a6cb316e9367d45ab05b0e535b0b31bb1b9
Maxim Alexandrov <maxim_0101@bk.ru>
2025-07-24 18:45:51 +0000

    Fix tests

--- tests/handlers/test_pack.py
+++ tests/handlers/test_pack.py
@@ -12,8 +12,11 @@ async def test_create_pack(service_client):
 
 
 async def test_get_pack(service_client):
-    created_pack = await create_pack(service_client, "test_title")
+    test_title = "test_title"
+
+    created_pack = await create_pack(service_client, test_title)
     getted_pack = await get_pack(service_client, created_pack["id"])
+
     assert created_pack == getted_pack
 
 
@@ -24,4 +27,8 @@ async def test_get_all_packs(service_client):
     all_packs = await get_all_packs(service_client)
 
     assert len(all_packs) == 2
-    
+    getted_first_pack = all_packs[0]
+    assert getted_first_pack == first_pack
+
+    getted_second_pack = all_packs[1]
+    assert getted_second_pack == second_pack
--- tests/helpers/endpoints.py
+++ tests/helpers/endpoints.py
@@ -18,16 +18,8 @@ async def create_pack(service_client, title: str):
 
 
 async def get_pack(service_client, uuid: str):
-    assert validate_uuid(uuid)
     response = await service_client.get(Routes.GET_PACK, params={'uuid': uuid})
-    assert response.status == 200
     json_response = response.json()
-
-    assert "id" in json_response
-    assert json_response["id"] is not None
-    assert validate_uuid(json_response["id"])
-    assert "title" in json_response
-    assert json_response["title"] is not None
 
     return json_response
 
