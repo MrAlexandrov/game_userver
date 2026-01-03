cba4215ca47ba020b51e9ba6fc554a875e6d55bf
Maxim Alexandrov <maxim_0101@bk.ru>
2026-01-03 13:29:21 +0300

    Add queries

--- src/queries/get_player_by_id.sql
+++ src/queries/get_player_by_id.sql
@@ -1,4 +1,3 @@
 SELECT id, game_session_id, name, score, joined_at
 FROM quiz.players
-WHERE game_session_id = $1
-ORDER BY joined_at ASC;
+WHERE id = $1;
--- src/queries/get_questions_and_variants_by_pack_id.sql
+++ src/queries/get_questions_and_variants_by_pack_id.sql
@@ -1,14 +1,13 @@
-SELECT
+SELECT 
     q.id AS question_id,
-    q.pack_id,
+    q.pack_id AS question_pack_id,
     q.text AS question_text,
-    q.image_url,
-    jsonb_agg(jsonb_build_object(
-        'id', v.id,
-        'text', v.text,
-        'is_correct', v.is_correct
-    )) AS variants
+    q.image_url AS question_image_url,
+    v.id AS variant_id,
+    v.question_id AS variant_question_id,
+    v.text AS variant_text,
+    v.is_correct AS variant_is_correct
 FROM quiz.questions q
-JOIN quiz.variants v ON q.id = v.question_id
+LEFT JOIN quiz.variants v ON q.id = v.question_id
 WHERE q.pack_id = $1
-GROUP BY q.id;
+ORDER BY q.id, v.id;
